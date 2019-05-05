d2cdc229794be60088c9c74daca6692ed7ba0e5a
btkramm <btkramm@uc.cl>
2019-05-05 19:53:15 -0400

    feat: Added fixtures

--- src/index.js
+++ src/index.js
@@ -4,28 +4,18 @@ import '@/assets/stylesheets/index.sass';
 import Product from '@/components/Product';
 import Rating from '@/components/Rating';
 
-window.onload = () => {
+// Fixtures
+import productFixtures from '@/components/Product/fixtures';
+import ratingFixtures from '@/components/Rating/fixtures';
+
+if ('content' in document.createElement('template')) {
   const root = document.getElementById('application');
 
-  if ('content' in document.createElement('template')) {
-    const product = Product({
-      discount: '38%',
-      image:
-        'https://media.tracfone.com/wps/contenthandler/dav/content/libraries/wcm.library.phones/components/STSAS367VCP/wcm.comps.image/st_ecom_large_1.png',
-      name: 'Nokia',
-      prices: {
-        creditCard: '49.990',
-        online: '59.990',
-        normal: '79.990',
-      },
-    });
+  const product = Product(productFixtures[0]);
+  root.appendChild(product);
 
-    root.appendChild(product);
-
-    const rating = Rating({ maxValue: 5, currentValue: 3 });
-
-    root.appendChild(rating);
-  } else {
-    console.error('HTML5 templates not supported!');
-  }
-};
+  const rating = Rating(ratingFixtures[0]);
+  root.appendChild(rating);
+} else {
+  console.error('HTML5 templates not supported!');
+}
